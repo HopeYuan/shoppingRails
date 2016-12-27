4d4aa23732b719e2ae3dcd89d2d6b08e5149c5a9
Xuanxuan Yuan <www.420426962.com@qq.com>
2016-12-27 20:08:12 +0000

    Skeleton User profile page support

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@
       validates :email, presence: true, 
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
-   has_secure_password
+  
    validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
     end    
