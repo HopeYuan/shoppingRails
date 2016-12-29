b5df3a29b4327f4f1703029ed6c319fb4aa0be28
Xuanxuan Yuan <www.420426962.com@qq.com>
2016-12-29 23:33:14 +0000

    improves product page and show information of products

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
        resources :users
        resources :charges
         resources :purchases,only: [:show]
+         resources :products,only: [:index]
     end
